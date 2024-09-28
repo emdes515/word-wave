48dbe508dd3aa7ade078ae547244f1f429e29d02
Mateusz Jankowski <mateuszhg5@gmail.com>
2024-09-28 23:46:33 +0200

    chore: Update next.config.mjs, .gitignore, capacitor.config.ts, tailwind.config.ts, and package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'com.example.app',
+	appName: 'word-wave',
+	webDir: 'out',
+	bundledWebRuntime: false,
+	server: {
+		url: 'http://localhost:3000',
+		cleartext: true,
+	},
+};
+
+export default config;
