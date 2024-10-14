6ec7949a1c2ad9981577368a1f88151ff8b5d7ec
Mateusz Jankowski <mateuszhg5@gmail.com>
2024-10-14 22:25:44 +0200

    chore: Remove unnecessary comments

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,10 +2,8 @@ import { redirect } from '@sveltejs/kit';
 import { SESSION_COOKIE, createSessionClient } from '$lib/server/appwrite.js';
 
 export async function load({ locals }) {
-	// Logged out users can't access this page.
 	if (!locals.user) redirect(302, '/login');
 
-	// Pass the stored user local to the page.
 	return {
 		user: locals.user
 	};
@@ -14,14 +12,11 @@ export async function load({ locals }) {
 // Define our log out endpoint/server action.
 export const actions = {
 	logout: async (event) => {
-		// Create the Appwrite client.
 		const { account } = createSessionClient(event);
 
-		// Delete the session on Appwrite, and delete the session cookie.
 		await account.deleteSession('current');
 		event.cookies.delete(SESSION_COOKIE, { path: '/' });
 
-		// Redirect to the sign up page.
 		redirect(302, '/login');
 	}
 };
